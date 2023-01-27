ffd263bf740437f46f4d26953efa72520cb6471b
Ruslan <105549663+russellsoft@users.noreply.github.com>
2023-01-27 18:10:11 +0300

    Add files via upload

---
+++ src/store/store.js
@@ -0,0 +1,29 @@
+import { compose, createStore, applyMiddleware } from 'redux'
+import { logger } from 'redux-logger'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/es/storage/session'
+import thunk from 'redux-thunk'
+
+import { rootReducer } from './root-reducer'
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger, thunk].filter(Boolean)
+
+const persistConfig = {
+	key: root,
+	storage,
+	whiteList: 'cart'
+}
+
+const composedEnchancer =
+	(process.env.NODE_ENV !== 'production' &&
+		window &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
+	|| compose
+
+const composedEnchancers = composedEnchancer(applyMiddleware(...middleWares))
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = createStore(persistedReducer, undefined, composedEnchancers)
+
+export const persistor = persistStore(store)
